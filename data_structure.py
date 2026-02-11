37fb185ee47cb1b3a9c34f8c17391df3ac4edbf3
timothyouu <tlou@csu.fullerton.edu>
2026-02-11 12:14:47 -0800

    set up all the functions and comments for the data_structure file

---
+++ data_structure.py
@@ -0,0 +1,49 @@
+# 1. Advanced List Operations:
+
+#add the item to the back of the list
+def append_item(my_list, item):
+
+#inserts an item at a specified index
+def insert_item(my_list, index, item):
+
+#removes the first occurence of the specified item
+def remove_item(my_list, item):
+
+#pops an item from the list at the given index
+def pop_item(my_list, index = -1):
+
+#clears all items from the list
+def clear_list(my_list):
+
+#sorts the list in ascending order
+def sort_list(my_list):
+
+#reverses the order of items in the list
+def reverse_list(my_list):
+
+#returns the index of the first occurence of the item
+def index_of_item(my_list, item):
+
+#counts how many times of the item appears in the list
+def count_item(my_list, item):
+
+#returns a slice of the list from start to end index
+def slice_list(my_list, start, end):
+
+#returns a list with the item removed using the 'del' keyword
+def delete_item(my_list, item):
+
+
+# 2. Stack and Queue Implementations:
+
+#add item to the stack
+def push_stack(stack, item):
+
+#remove item from the stack
+def pop_stack(stack):
+
+#add item to the queue
+def enqueue(queue, item):
+
+#remove an element of a queue
+def dequeue(queue):
